7f6f26863b278fa71dd1c072f2aae984ef8cba09
Coocko <krabik4794@gmail.com>
2021-03-06 22:40:30 +0200

    feat: add solution. 12 passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let open = [];
+  let close = [];
+  for(let i = 0 ; i < bracketsConfig.length;i++){
+    open.push(bracketsConfig[i][0])
+    close.push(bracketsConfig[i][1])
+  }
+  for( let i =0 ; i < str.length; i++) {
+    if(open.indexOf(str[i]) !== -1 ) stack.push(str[i]);
+    else if(close.indexOf(str[i]) !== -1 ) stack.pop(str[i]);
+  }
+  return stack.length === 0 ? true : false;
 }
